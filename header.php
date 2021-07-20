c2d151397e243523e2347ff1cf1024020216c8a5
piyushsinha17 <61818815+piyushsinha17@users.noreply.github.com>
2021-07-20 21:06:10 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,24 @@
+<!-- <!DOCTYPE html> -->
+<!-- <html lang="en"> -->
+<!-- <head>
+    <title>This Is My Website</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik&display=swap rel="stylesheet">
+</head>
+<body> -->
+<header>
+        <section class="navsection">
+            <div class="logo">
+                <h1>Piyush Sinha</h1>
+            </div>
+            <nav>
+                <a href="https://twitter.com/piyushmaybe"target="_blank" >Home</a>
+                <a href="pages/services.php" target="_blank">Services</a>
+                <a href="https://twitter.com/piyushmaybe"target="_blank" >Gallery</a>
+                <a href="https://twitter.com/piyushmaybe"target="_blank" >Portfolio</a>
+                <a href="https://twitter.com/piyushmaybe"target="_blank" >About</a>
+                <a href="https://twitter.com/piyushmaybe"target="_blank" >Contact</a>
+            </nav>
+        </section>
+</header>
+        <!-- </body>
+        </html> -->
---
+++ pages/services.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Services </title>
+</head>
+<body>
+    <?php include('../header.php')?>
+    <main>
+        <div class="leftside">
+        <h3>Hello</h3>
+        <h1>I am Piyush Sinha</h1>
+        <h2>Web Devloper And Editor</h2>
+        <a href="#" class="btn1">Hire Me</a>
+        <a href="#" class="btn2">Contact Me</a>
+        </div>
+        <div class="rightside">
+        <img src="img/img.png" alt="" class="">
+  
+        </div>
+
+
+        </main>
+</body>
+</html>
